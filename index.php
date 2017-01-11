944a646d7f6326957b9e7873fe048eb8e6a67243
Eries Trisnadi <zdumb1885@outlook.com>
2017-01-12 02:03:57 +0700

    update index.php dan lib/class.php

--- index.php
+++ index.php
@@ -1,46 +1,43 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>STMIK SUBANG KRS Online</title>
-
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <style media="screen">
-      div.container{ margin-top: 20px;}
-    </style>
-  </head>
-  <body>
+<?php
+// include class
+include_once 'lib/class.php';
+if(isset($_SESSION['login_user']) && isset($_SESSION['login_pass'])){
+  $user->doLogin($_SESSION['login_user'], $_SESSION['login_pass']);
+}else{
+  if(isset($_POST['nomor_induk']) && isset($_POST['password'])){
+  $options = [
+      'cost' => 11,
+  ];
+  $user->doLogin($_POST['nomor_induk'], $_POST['password']);
+  }
+}
+// Judul page
+$page_title = 'Masuk';
+include_once 'temp/header.php'; ?>
     <div class="container">
-      <form class="card card-block">
+      <form class="card card-block" method="post">
         <div class="form-group row">
-          <div class="col-sm-6 offset-sm-5">
-            <h1 class="display-3">Sign in!</h1>
+          <div class="col-sm-8 offset-sm-2">
+            <h1 class="display-4 text-uppercase text-md-center text-sm-center text-xs-center"><?= $site_name; ?></h1>
           </div>
         </div>
         <div class="form-group row">
           <label for="inputNomorInduk" class="col-sm-2 offset-sm-2 form-control-label">Nomor Induk</label>
           <div class="col-sm-6">
-            <input type="text" class="form-control" id="inputNomorInduk" placeholder="Nomor Induk">
+            <input type="text" class="form-control" id="inputNomorInduk" name="nomor_induk" placeholder="Nomor Induk">
           </div>
         </div>
         <div class="form-group row">
           <label for="inputPassword" class="col-sm-2 offset-sm-2 form-control-label">Password</label>
           <div class="col-sm-6">
-            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+            <input type="password" class="form-control" id="inputPassword" name="password" placeholder="Password">
           </div>
         </div>
         <div class="form-group row">
           <div class="offset-sm-4 col-sm-10">
-            <button type="submit" class="btn btn-lg btn-primary">Sign in</button>
+            <button type="submit" class="btn btn-lg btn-primary">Masuk</button>
           </div>
         </div>
       </form>
     </div>
-
-    <script src="assets/js/jquery-3.1.1.min.js"></script>
-    <script src="assets/js/tether.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php include_once 'temp/footer.php'; ?>
--- lib/class.php
+++ lib/class.php
@@ -1,7 +1,12 @@
-class MyClass
-{
-  //Class Properties and methods go here
-}
+<?php
+session_start();
+include_once 'site.php';
+include_once 'database.php';
+include_once 'user.php';
 
-$obj= new MyClass;
-var_dump($obj);
+// Koneksi database
+$database = new Database();
+$db = $database->getConnection();
+
+// Initial database ke class
+$user = new User($db, $base_url);
