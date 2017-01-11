2ac8167bcba6f67333fda4a0ab5b51be335eaffe
Eries Trisnadi <zdumb1885@outlook.com>
2017-01-12 02:00:31 +0700

    tambah template header dan footer

---
+++ temp/footer.php
@@ -0,0 +1,5 @@
+    <script src="<?= $base_url; ?>assets/js/jquery-3.1.1.min.js"></script>
+    <script src="<?= $base_url; ?>assets/js/tether.min.js"></script>
+    <script src="<?= $base_url; ?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ temp/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?= $site_name . ' - ' . $page_title; ?></title>
+
+    <link rel="stylesheet" href="<?= $base_url; ?>assets/css/bootstrap.min.css">
+    <style media="screen">
+      div.container{ margin-top: 20px;}
+    </style>
+  </head>
+  <body>
